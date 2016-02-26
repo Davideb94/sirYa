f88145d2958148648a5838f0787f508d16ed8d8f
Davide Brunetti <davide.brunetti@novetica.org>
2016-02-26 05:47:10 +0000

    Started searches.html

--- src/app/ui/app/searches/searches.js
+++ src/app/ui/app/searches/searches.js
@@ -5,7 +5,13 @@
 	 * ui app module.
 	 */
 	angular.module('sirya.ui.app.searches', [])
-	.controller('SearchesCtrl', function($scope) {
-		console.log("searches contorller");
+	.controller('SearchesCtrl', function($scope, rest) {
+		var self = this;
+        // self.user = sirya.ui.common.services.api.returnUser();
+        self.user = rest.getUserWithId();
+        self.searches = rest.getSearchesFromUserId();
+
+        console.log( rest.getUserWithId() );
+        console.log( rest.getSearchesFromUserId() );
 	});
 })(window.angular);
